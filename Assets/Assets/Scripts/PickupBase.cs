21ec7269774cfa763d06ae6a6c915361dd71baef
KarlKoehle <43576463+KarlKoehle@users.noreply.github.com>
2022-01-20 11:17:19 -0600

    Add Inheritance : PickupBase/Fish
    consolidate pickups to abstract class

---
+++ Assets/Assets/Scripts/PickupBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PickupBase : MonoBehaviour
+{
+    public GameObject Fish;
+    public SokobanPlayerMove Player;
+    public AudioSource playSound;
+
+    private void Start()
+    {
+        Player = FindObjectOfType<SokobanPlayerMove>();
+    }
+
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        if (hit.CompareTag("Player"))
+        {
+            OnPickup();
+
+            playSound.Play();
+            Destroy(gameObject);
+            print("Fish has been collected");
+        }
+    }
+
+    public abstract void OnPickup(); 
+   
+}
+
+
--- Assets/Assets/Scripts/PickupFish.cs
+++ Assets/Assets/Scripts/PickupFish.cs
@@ -2,26 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickupFish : MonoBehaviour
+public class PickupFish : PickupBase
 {
-    public GameObject Fish;
-    public SokobanPlayerMove Player;
     public int points = 10;
-    public AudioSource playSound;
 
-    private void Start() {
-        Player = FindObjectOfType<SokobanPlayerMove>();
-    }
-
-    void OnTriggerEnter2D(Collider2D hit)
+    public override void OnPickup()
     {
-        if (hit.CompareTag("Player"))
-        {
-            Player.points += points;
-            playSound.Play();
-            Destroy(gameObject);
-            print("Fish has been collected");
-        }
+        Player.points += points;
     }
 }
 
